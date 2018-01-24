528c1319657b8f742d6ae9ae65e3d33df87eef9d
Donutcat <31439092+donutcatscience@users.noreply.github.com>
2018-01-23 17:39:02 -0700

    Three Hour Update
    Added:
    walls with collision
    prefab bricks
    physics to ball and walls
    first level bricks

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    public Paddle paddle;
+
+    private bool hasStarted = false;
+    private Vector3 paddleToBallVector;
+
+	// Use this for initialization
+	void Start () {
+        paddleToBallVector = this.transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!hasStarted)
+        {
+            //lock ball on top of the paddle until mouse press
+            this.transform.position = paddle.transform.position + paddleToBallVector;
+            if (Input.GetMouseButtonDown(0))
+            {
+                hasStarted = true;
+                this.GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 10f);
+            }
+        }
+
+
+	}
+}
